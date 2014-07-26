217bd061b5b2c3d7af874de1dbd6e1a00ce5b810
QiuFeng <qfviolethill@163.com>
2014-07-27 01:00:18 +0800

    update TJFA empty sql

--- empty database/TJFA.sql
+++ empty database/TJFA.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 04, 2014 at 05:23 AM
+-- Generation Time: Jul 26, 2014 at 04:58 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.9
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `Competition` (
   `competitionId` int(32) NOT NULL AUTO_INCREMENT COMMENT '赛事id',
-  `isStart` smallint(5) NOT NULL DEFAULT '0' COMMENT '赛事是否开始 0未开始的赛事 1已经开始 2已经结束',
+  `isStart` smallint(5) NOT NULL DEFAULT '0' COMMENT '赛事是否开始 0未开始的赛事 1正在进行中 2已经结束',
   `name` varchar(400) COLLATE utf8_bin NOT NULL COMMENT '赛事的名字  如  足协杯',
   `number` int(32) NOT NULL COMMENT '第几届比赛',
   `time` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '赛事时间 20131与20132表示第一学期和第二学期',
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `Match` (
   `matchId` int(32) NOT NULL AUTO_INCREMENT COMMENT '比赛ID',
   `date` datetime NOT NULL COMMENT '比赛时间  如 2014年6月5日 12:15',
   `isStart` smallint(5) NOT NULL COMMENT '是否开始 0表示 没有开始  1或者2表示结束',
-  `matchProperty` smallint(5) NOT NULL COMMENT '比赛性质 小组赛或者淘汰赛 0小组赛 1淘汰赛',
+  `matchProperty` smallint(5) NOT NULL COMMENT '比赛性质 0:小组赛  1:决赛 2:半决赛 4:1/4 8:1/8 16:1/16 32:1/32 100：附加赛 ',
   `scoreA` int(32) NOT NULL COMMENT '比分 第一支球队的得分',
   `scoreB` int(32) NOT NULL COMMENT '比分 第二只球队的得分',
   `winTeamId` int(32) NOT NULL COMMENT '获胜队伍的 id',
@@ -67,6 +67,7 @@ CREATE TABLE IF NOT EXISTS `News` (
   `title` varchar(400) COLLATE utf8_bin NOT NULL COMMENT '新闻标题',
   `content` longtext COLLATE utf8_bin NOT NULL COMMENT '新闻内容 直接换成html文件 我们第一版先不做图片功能了',
   `date` date NOT NULL COMMENT '发布日期',
+  `precontent` varchar(1000) COLLATE utf8_bin NOT NULL COMMENT '新闻预览内容，纯文本 没有大小',
   PRIMARY KEY (`newsId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
@@ -102,8 +103,9 @@ CREATE TABLE IF NOT EXISTS `Team` (
   `missCount` int(32) NOT NULL COMMENT '失球数',
   `groupNo` int(32) NOT NULL COMMENT '所在小组  a b c d ',
   `name` varchar(400) COLLATE utf8_bin NOT NULL COMMENT '队伍名字',
-  `score` int(32) NOT NULL COMMENT '小组赛积分',
+  `score` int(32) NOT NULL COMMENT '小组赛积分 胜3 平1 输0',
   `competitionId` int(32) NOT NULL COMMENT '所属于的赛事 ',
+  `rank` int(32) NOT NULL COMMENT '最高排名  1234 前4名   8 八强  16 16强 0小组赛 100附加赛',
   PRIMARY KEY (`teamId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
